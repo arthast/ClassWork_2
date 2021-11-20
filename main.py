05bfe194ef21bb678b266aa7b4ce9799a69ab47c
arthast <arsentakhunov@gmail.com>
2021-11-20 23:30:47 +0300

    desain and main code2.0

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import random
 
 from PyQt5 import uic
 from PyQt5.QtCore import QPoint, Qt
-from PyQt5.QtGui import QPainter, QColor, QPolygon
+from PyQt5.QtGui import QPolygon, QColor, QPainter
 from PyQt5.QtWidgets import QApplication, QMainWindow
 
 
@@ -12,53 +12,35 @@ class MyWidget(QMainWindow):
         super().__init__()
         uic.loadUi('untitled.ui', self)  # Загружаем дизайн
         self.pushButton.clicked.connect(self.run)
-        # Обратите внимание: имя элемента такое же как в QTDesigner
+        self.ok = False
 
-    def initUI(self):
-        self.setGeometry(600, 600, 600, 600)
-        self.setWindowTitle('Супрематизм')
-        self.x = 0
-        self.y = 0
-        self.shape = -1
-        self.setMouseTracking(True)
+    def run(self):
+        self.ok = True
+        self.update()
 
     def paintEvent(self, event):
-        self.qp = QPainter()
-        self.qp.begin(self)
-        self.qp.setBrush(QColor(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)))
-        if self.shape == 0:
-            self.qp.drawEllipse(self.x, self.y, random.randint(30, 255), random.randint(30, 255))
-        elif self.shape == 1:
-            self.qp.drawRect(self.x, self.y, random.randint(30, 255), random.randint(30, 255))
-        elif self.shape == 2:
-            self.qp.drawPolygon(QPolygon([QPoint(self.x, self.y),
-                                          QPoint(self.x - random.randint(30, 255),
-                                                 self.y + random.randint(30, 255)),
-                                          QPoint(self.x + random.randint(30, 255),
-                                                 self.y + random.randint(30, 255))]))
-        self.qp.end()
+        if self.ok:
+            self.qp = QPainter()
+            # Начинаем процесс рисования
+            self.qp.begin(self)
+            self.draw_flag()
+            self.qp.end()
 
-    def keyPressEvent(self, event):
-        if event.key() == Qt.Key_Space:
-            self.shape = 2
-            self.repaint()
+    def draw_flag(self):
+        # Задаем кисть
+        size = random.randint(0, 250)
+        self.qp.setBrush(QColor('yellow'))
+        self.qp.drawEllipse(130, 200, size, size)
+        # Рисуем прямоугольник заданной кистью
 
-    def mousePressEvent(self, event):
-        self.x = event.x()
-        self.y = event.y()
-        if event.button() == Qt.LeftButton:
-            self.shape = 1
-        elif event.button() == Qt.RightButton:
-            self.shape = 0
-        self.repaint()
 
-    def mouseMoveEvent(self, event):
-        self.x = event.x()
-        self.y = event.y()
+def except_hook(cls, exception, traceback):
+    sys.excepthook(cls, exception, traceback)
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
+    sys.excepthook = except_hook
     ex = MyWidget()
     ex.show()
     sys.exit(app.exec_())
